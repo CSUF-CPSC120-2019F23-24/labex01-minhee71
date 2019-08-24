dbae355c1a97676552a161a858a3494efa98cf12
minhee71 <minhee71@cs104u-6d.acad.fullerton.edu>
2019-08-24 14:21:29 -0700

    Girls go to Mars to get more candy bars. Boys go to Jupiter to get more stupider.

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -4,21 +4,22 @@
 
 int main()
 {
-  double hours, rate, pay;
+  double bars, price, earned;
 
   // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
-  std::cin >> hours;
+  std::cout << "Welcome to the Earnings Calulcator! " << std::endl;
+  std::cout << "How many candy bars did you sell? ";
+  std::cin >> bars;
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
-  std::cin >> rate;
+  std::cout << "How much does the organization earn for each bar sold? ";
+  std::cin >> price;
 
   // Calculate the pay.
-  pay = hours * rate;
+  earned = bars * price;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
+  std::cout << "Congratulations! You have earned $" << earned << " for the organization!" << std::endl;
 
   return 0;
 }
